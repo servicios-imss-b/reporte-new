311db6ac72ee49e680efbc75cf84cae44bbe520c
jose-valdez_IMSSB <jose-valdez_IMSSB@users.noreply.github.com>
2026-07-23 12:23:27 -0600

    fix: corrige base path para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/reporte-_angeles/' : '/',
+  base: mode === 'production' ? '/reporte-new/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
